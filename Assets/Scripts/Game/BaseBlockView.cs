a27a4c9aa82c909fe0c4663363d7254a0dec9fc6
Fox <Xismatulin.ru@yandex.ru>
2020-11-22 04:16:15 +0500

    Прототип Game игры

---
+++ Assets/Scripts/Game/BaseBlockView.cs
@@ -0,0 +1,74 @@
+﻿using DG.Tweening;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace FizreFox
+{
+    public class BaseBlockView : MonoBehaviour
+    {
+        public Action OnClick = delegate { };
+
+        [SerializeField]
+        private Image _image;
+
+        [SerializeField]
+        private Button _button;
+
+        private bool _interacleble;
+        private bool _isFlying;
+        private float _speed = 500f;
+        private float _lifeTime = 10f;
+        private float _flyingTime;
+
+
+        public void StartFly()
+        {
+            _isFlying = true;
+            _interacleble = true;
+            _image.DOFade(1, 0);
+            _flyingTime = 0;
+        }
+
+        public void StopFly()
+        {
+            _isFlying = false;
+        }
+
+        public void SetSpeed(float value)
+        {
+            _speed = value;
+        }
+
+        private void Start()
+        {
+            _interacleble = true;
+            _button.onClick.AddListener(OnButtonClick);
+        }
+
+
+        private void Update()
+        {
+            if (_isFlying)
+            {
+                transform.localPosition += new Vector3(0, -1, 0) * _speed * Time.deltaTime;
+                _flyingTime += Time.deltaTime;
+
+                if (_flyingTime >= _lifeTime) 
+                    StopFly();
+            }
+        }
+
+        private void OnButtonClick()
+        {
+            if (_interacleble)
+            {
+                OnClick.Invoke();
+                _image.DOFade(.5f, .3f).SetEase(Ease.InOutSine);
+                _interacleble = false;
+            }
+        }
+    }
+}
--- Assets/Scripts/Lobby/GameController.cs
+++ Assets/Scripts/Lobby/GameController.cs
@@ -2,12 +2,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 using UnityEngine.UI;
+using TMPro;
 
 namespace FizreFox
 {
+    [Serializable]
+    public class SongModel<TK, TV>
+    {
+        [SerializeField] public TK Key;
+        [SerializeField] public TV Value;
+    }
+
     public class GameController : MonoBehaviour
     {
+        [SerializeField]
+        private TextMeshProUGUI _counter;
+
         [SerializeField]
         private Button _startButton;
 
@@ -17,8 +29,25 @@ namespace FizreFox
         [SerializeField]
         private AudioClip _mainClip;
 
+        [SerializeField]
+        private BaseBlockView _blockPrefab;
+
+        [SerializeField]
+        private List<Transform> _spawners = new List<Transform>();
+
+        [SerializeField]
+        public List<SongModel<float, int>> _songModel = new List<SongModel<float, int>>();
+
+        private List<BaseBlockView> _fallingObjects = new List<BaseBlockView>();
+        private int _score;
+
         void Start()
         {
+            _songModel.AddRange(_songModel);
+            _songModel.AddRange(_songModel);
+            _songModel.AddRange(_songModel);
+            _songModel.AddRange(_songModel);
+
             _startButton.onClick.AddListener(StartGame);
         }
 
@@ -27,6 +56,47 @@ namespace FizreFox
             _audioSource.clip = _mainClip;
             _audioSource.Play();
             _startButton.GetComponent<Image>().DOFade(0,1f);
+
+            StartCoroutine(PlaySongRoutine());
+        }
+
+        private IEnumerator PlaySongRoutine()
+        {
+            float lastPoint = 0;
+            foreach (var point in _songModel)
+            {
+                yield return new WaitForSeconds(point.Key - lastPoint);
+                lastPoint = point.Key;
+                InsertBlock(point.Value);
+            }
+        }
+
+        private void InsertBlock(int track)
+        {
+            if (_fallingObjects.Count > 20)
+            {
+                var pulledBlock = _fallingObjects[0];
+                _fallingObjects.Remove(pulledBlock);
+                pulledBlock.StopFly();
+                pulledBlock.transform.SetParent(_spawners[track]);
+                pulledBlock.transform.localPosition = Vector3.zero;
+                pulledBlock.StartFly();
+                _fallingObjects.Add(pulledBlock);
+            }
+            else
+            {
+                var newBlock = Instantiate(_blockPrefab, _spawners[track]);
+                newBlock.transform.localPosition = Vector3.zero;
+                newBlock.StartFly();
+                newBlock.OnClick += UpdateCounter;
+                _fallingObjects.Add(newBlock);
+            }
+        }
+
+        private void UpdateCounter()
+        {
+            _score++;
+            _counter.text = _score.ToString();
         }
     }
 }
