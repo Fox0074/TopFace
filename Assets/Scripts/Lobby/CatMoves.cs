d3aa915f22a4e3f440dad64ce0273068471978f6
Fox <Xismatulin.ru@yandex.ru>
2020-11-23 22:34:57 +0500

    Новый кот + Анимации

--- Assets/Scripts/Lobby/CatMoves.cs
+++ Assets/Scripts/Lobby/CatMoves.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Playables;
 using UnityEngine.UI;
 
 namespace FizreFox.Meta
