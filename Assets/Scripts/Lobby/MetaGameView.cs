5a35ce985b33201f1021c7721a03ec1074382a07
Fox <Xismatulin.ru@yandex.ru>
2020-12-06 17:48:50 +0500

    MetaGameMediator

---
+++ Assets/Scripts/Lobby/MetaGameView.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace FizerFox.Meta
+{
+    public class MetaGameView : MonoBehaviour
+    {
+        private const string LobbyMusicSoundId = "lobby_music";
+        private const string BoughtSoundId = "buy_any_item";
+
+        //[Inject]
+        //private IAudioPlayer _audioPlayer;
+
+        public void Initialize()
+        {
+            //var musicAudioId = _audioPlayer.PlayMusic(LobbyMusicSoundId, true);
+            //_audioPlayer.SetVolume(musicAudioId, 0f);
+            //_audioPlayer.Fade(musicAudioId, 1f, 2f);
+        }
+
+        public void ItemBought()
+        {
+            //_audioPlayer.Play(BoughtSoundId);
+        }
+    }
+}
---
+++ Assets/Scripts/Lobby/MetaGameViewMediator.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Zenject;
+
+namespace FizerFox.Meta
+{
+    public class MetaGameViewMediator : Mediator<MetaGameView>
+    {
+        //[Inject]
+        //private MetaGame _metaGame;
+
+        //[Inject]
+        //private Player _player;
+
+        //[Inject]
+        //private ApplicationSettings _settings;
+
+        [Inject]
+        private SignalBus _signalBus;
+
+        private static int _lastCrownEventStage;
+        private bool _sceneAppeared;
+
+        public override void OnRegister()
+        {
+            _signalBus.Subscribe<SceneAppearSignal>(OnSceneAppear);
+            View.Initialize();
+        }
+
+        public override void OnRemove()
+        {
+            _signalBus.Unsubscribe<SceneAppearSignal>(OnSceneAppear);
+        }
+
+        private void OnSceneAppear()
+        {
+            _sceneAppeared = true;
+
+            if (TryShowGameWindows())
+                return;
+        }
+
+        private bool TryShowGameWindows()
+        {
+            var windowsStack = GetGameWindows().ToArray();
+
+            for (var i = 0; i < windowsStack.Length; i++)
+            {
+                var item = windowsStack[i];
+                item.Value.Secondary = true;
+                item.Value.Stacked = i < (windowsStack.Length - 1);
+                item.Value.WaitForAnimations = true;
+                _signalBus.Fire<PushWindowSignal>(new PushWindowSignal(item.Key, item.Value));
+            }
+
+            return windowsStack.Length > 0;
+        }
+
+        // WARN: не стоит тут использовать WindowOptions.Empty
+        private IEnumerable<KeyValuePair<Type, WindowOptions>> GetGameWindows()
+        {
+            //    if (_player.DailyBonusExist())
+            //    {
+            //        //_player.TryBreakDailyBonus();
+
+            //        yield return new KeyValuePair<Type, WindowOptions>(
+            //            typeof(DailyBonusWindow),
+            //            new DailyBonusWindowOptions()
+            //            {
+            //                DailyBonuses = _player.GetDailyBonuses(),
+            //                CurrentDay = _player.DailyBonusCurrentDay
+            //            }
+            //        );
+            //    }
+
+
+            if (false)
+            {
+                yield return new KeyValuePair<Type, WindowOptions>();
+            }
+        }
+
+        private void OnHardCurrencyLack()
+        {
+
+        }
+
+        private void OnHealthLack()
+        {
+
+        }
+
+        private void OnNetworkLack()
+        {
+
+        }
+
+        private void OnLevelFetchingFinished()
+        {
+
+        }
+
+        private void OnLevelFetchingFailed()
+        {
+
+        }
+    }
+}
